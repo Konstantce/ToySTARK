23cac2ca12f97bd127340e8e2714ee4a2a71555c
konstantce <kopanarin@yandex.ru>
2019-07-02 14:49:33 +0300

    work on FRI-OPP

--- FRI-OPP/FRI-OPP.py
+++ FRI-OPP/FRI-OPP.py
@@ -1,1 +1,12 @@
-def FRI_OPP
+from domain import *
+
+class FRI_OPP():
+    def __init__(domain):
+        self.domain = domain
+
+    #we do construct a Merkle tree as a output
+    def
+
+    def setup_ier(poly, x, stage_num):
+
+    
