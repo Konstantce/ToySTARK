2450003461ded734444dff1b13dbed1763b0d9f9
konstantce <kopanarin@yandex.ru>
2019-07-02 17:03:42 +0300

    with hash to field

--- FRI-OPP/FRI-OPP.py
+++ FRI-OPP/FRI-OPP.py
@@ -1,12 +1,13 @@
 from domain import *
+from merkle_tree.merkle_tree import *
+from merkle_tree.hash_to_field import *
 
 class FRI_OPP():
     def __init__(domain):
         self.domain = domain
 
     #we do construct a Merkle tree as a output
-    def
-
+    #TODO: balance the tree by corresponding cosets
     def setup_ier(poly, x, stage_num):
 
     
